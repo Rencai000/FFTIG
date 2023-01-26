499f1aef84b6dfba374006c059949d762e3d51f4
ZeniBot <122364236+Rencai000@users.noreply.github.com>
2023-01-26 11:14:33 +0700

    Add files via upload

---
+++ src/info.js
@@ -0,0 +1,28 @@
+const { chalk, inquirer, _, fs, instagram, print, delay } = require("./api.js");
+
+print(
+                    chalk`{bold.yellow
+  A simple NodeJS-based tool to help increase engagement on Instagram accounts and interact between accounts effortlessly. This tool was recoded from I Putu Jaya Adi Pranata.
+
+  
+  Find Bug ? Tell me on
+  —————————————————————————————————————————————————————
+  
+▒███████▒▓█████  ███▄    █  ██▓ ▄▄▄▄    ▒█████  ▄▄▄█████▓
+▒ ▒ ▒ ▄▀░▓█   ▀  ██ ▀█   █ ▓██▒▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒
+░ ▒ ▄▀▒░ ▒███   ▓██  ▀█ ██▒▒██▒▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░
+  ▄▀▒   ░▒▓█  ▄ ▓██▒  ▐▌██▒░██░▒██░█▀  ▒██   ██░░ ▓██▓ ░ 
+▒███████▒░▒████▒▒██░   ▓██░░██░░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░ 
+░▒▒ ▓░▒░▒░░ ▒░ ░░ ▒░   ▒ ▒ ░▓  ░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░   
+░░▒ ▒ ░ ▒ ░ ░  ░░ ░░   ░ ▒░ ▒ ░▒░▒   ░   ░ ▒ ▒░     ░    
+░ ░ ░ ░ ░   ░      ░   ░ ░  ▒ ░ ░    ░ ░ ░ ░ ▒    ░      
+  ░ ░       ░  ░         ░  ░   ░          ░ ░           
+░                                    ░                   
+  —————————————————————————————————————————————————————
+  
+  Special thanks to :
+  
+  - I Putu Jaya Adi Pranata
+  - Raihan Ramadhani
+  - All members of SGB Team
+  \n}`);
